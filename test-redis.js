e37eec6cece53a52dfdb75c45caddfd3fd894b08
Hong Kun Tian <hongkun.tian@hotmail.com>
2025-08-03 22:12:09 -0700

    Fix redis with new env_vars

--- test-redis.js
+++ test-redis.js
@@ -2,8 +2,8 @@ import { Redis } from "@upstash/redis";
 import "dotenv/config";
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.REDIS_URL,
+  token: process.env.KV_REST_API_TOKEN,
 });
 
 async function testRedisConnection() {
